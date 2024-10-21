331408cb30b8184607bd67e93cceb48ee16ea225
Sung-Lin Hsieh <122555461+hsiehs2023@users.noreply.github.com>
2024-10-20 22:29:30 -0500

    add firebase template

---
+++ firebase_template.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+}); 
